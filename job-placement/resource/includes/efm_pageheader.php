a29a0f91b5cad82b56acb9f5af100df4aec65b72
codergaint <71995368+codergaint@users.noreply.github.com>
2025-03-10 08:23:00 +0600

    updateApp

--- job-placement/resource/includes/efm_pageheader.php
+++
@@ -1,6 +0,0 @@
-	 <!-- Page Header-->
-          <header class="page-header">
-            <div class="container-fluid">
-              <p class="no-margin-bottom"><i class="fa fa-arrow-circle-o-right"></i> You are here - <?php if($page=="Dashboard"){echo "Dashboard";}else{ echo "Dashboard"; ?> | <?php echo $page;} ?></p>
-            </div>
-          </header>
--- job-placement/resource/includes/placement_pageheader.php
+++
@@ -1,6 +0,0 @@
-	 <!-- Page Header-->
-          <header class="page-header">
-            <div class="container-fluid">
-              <p class="no-margin-bottom"><i class="fa fa-arrow-circle-o-right"></i> Job Placement Information</p>
-            </div>
-          </header>
