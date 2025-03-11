0306899120661ce229df97ff3bb47d454a37a6dd
codergaint <71995368+codergaint@users.noreply.github.com>
2025-03-11 22:49:00 +0600

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\View;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,8 +20,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+<<<<<<< Updated upstream
         View::composer('*', function($view){
             $view->share('website_name', 'My website');
+=======
+        View::composer('*', function ($view) {
+            $view->with('website', 'sbc');
+>>>>>>> Stashed changes
         });
     }
 }
