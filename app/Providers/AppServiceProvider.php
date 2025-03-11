4cf25e02ef0067c0b43d41afa4562283f378e9a9
codergaint <71995368+codergaint@users.noreply.github.com>
2025-03-11 22:52:53 +0600

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,18 +20,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
         View::composer('*', function($view){
             $view->share('website_name', 'My website');
-=======
-        View::composer('*', function ($view) {
-            $view->with('website', 'sbc');
->>>>>>> Stashed changes
-=======
-        View::composer('*', function ($view) {
-            $view->with('website', 'sbc');
->>>>>>> Stashed changes
         });
     }
 }
